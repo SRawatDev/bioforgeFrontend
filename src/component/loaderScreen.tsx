4f43f3d5452471653cadcb1f9bb053aa2378a2f4
sumit rawat <sumit@braintechnosys.com>
2025-07-14 16:56:47 +0530

    add loader

--- src/component/loaderScreen.tsx
+++ src/component/loaderScreen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 const LoadScreen: React.FC = () => (
   <div className="fixed inset-0 flex flex-col items-center justify-center  z-50 loading">
-    <img src="/src/assets/loader.gif" alt="logo" />
+    <img src="/assets/loader.gif" alt="logo" />
   </div>
 );
 export default LoadScreen;

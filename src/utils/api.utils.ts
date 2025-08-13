f9b40db7259343a14dca25c2050697b7b0fafb7e
bttrainees123 <bttrainees1@gmail.com>
2025-08-13 17:28:41 +0530

    update after pull

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -31,7 +31,8 @@ export const apiUrls = {
     statuslinkCategory:"api/v1/status-linkCategory", 
     deletelinkCategory:"api/v1/delete-linkCategory" ,
     //subsciber
-    getAllsubscribe:"api/v1/getAll-subscribe" 
+    getAllsubscribe:"api/v1/getAll-subscribe",
+    createsession:"api/payment/create-checkout-session"
 
 
 };
